f22855194c47e9e6d9513dac8c95eb2f4fb43b65
54x1 <five4x1@gmail.com>
2021-10-11 07:54:22 +1100

    please work

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -40,182 +40,181 @@ const custom_theme = createTheme({
   },
 })
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
-
-  handleClick = api => e => {
-    e.preventDefault()
-
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
+class App extends Component {
 
   render() {
-    const { loading, msg } = this.state
-
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      <p>
-    )
-  }
-}
-
-class App extends Component {
-  // render() {
-  // return (
-    <div className='App'>
-      <ThemeProvider theme={custom_theme} className='container'>
-        <Router>
-          <Switch>
-            <Route
-              exact
-              path='/login'
-              component={() => (
-                <Login
-                  setCircle={(value) => setCircle(value)}
-                  setSnackbar={(value) => setSnackbar(value)}
-                />
-              )}
-            />
-            <Route
-              exact
-              path='/register'
-              component={() => (
-                <Register
-                  setCircle={(value) => setCircle(value)}
-                  setSnackbar={(value) => setSnackbar(value)}
-                />
-              )}
-            />
-            <Route
-              exact
-              path='/'
-              component={() => (
-                <Home
-                  setCircle={(value) => setCircle(value)}
-                  setSnackbar={(value) => setSnackbar(value)}
-                />
-              )}
-            />
-            <Route
-              exact
-              path='/user/:userId'
-              component={() => (
-                <UserProfile
-                  setCircle={(value) => setCircle(value)}
-                  setSnackbar={(value) => setSnackbar(value)}
-                />
-              )}
-            />
-            <Route
-              exact
-              path='/uni/:uniId'
-              component={() => (
-                <UniProfile
-                  setCircle={(value) => setCircle(value)}
-                  setSnackbar={(value) => setSnackbar(value)}
-                />
-              )}
-            />
-            <Route
-              path='/club/:clubId'
-              component={() => (
-                <Club
-                  setCircle={(value) => setCircle(value)}
-                  setSnackbar={(value) => setSnackbar(value)}
-                />
-              )}
-            />
-            <Route
-              exact
-              path='/club'
-              component={() => (
-                <Club
-                  setCircle={(value) => setCircle(value)}
-                  setSnackbar={(value) => setSnackbar(value)}
-                />
-              )}
-            />
-            <Route
-              exact
-              path='/studygroup/:studygroupId'
-              component={() => (
-                <StudyGroup
-                  setCircle={(value) => setCircle(value)}
-                  setSnackbar={(value) => setSnackbar(value)}
-                />
-              )}
-            />
-            <Route
-              exact
-              path='/market'
-              component={() => (
-                <Market
-                  setCircle={(value) => setCircle(value)}
-                  setSnackbar={(value) => setSnackbar(value)}
-                />
-              )}
-            />
-            <Route
-              exact
-              path='/market/:itemId'
-              component={() => (
-                <Market
-                  setCircle={(value) => setCircle(value)}
-                  setSnackbar={(value) => setSnackbar(value)}
-                />
-              )}
-            />
-            <Route
-              exact
-              path='/messenger'
-              component={() => (
-                <Messenger
-                  setCircle={(value) => setCircle(value)}
-                  setSnackbar={(value) => setSnackbar(value)}
-                />
-              )}
-            />
-            <Route exact path='/settings' component={Settings} />
-            <Route exact path='/error' component={Error} />
-          </Switch>
-        </Router>
-      </ThemeProvider>
-      <Backdrop
-        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-        open={circle}
-      >
-        <CircularProgress color='inherit' />
-      </Backdrop>
-      <Snackbar
-        anchorOrigin={{
-          vertical: 'top',
-          horizontal: 'center',
-        }}
-        open={snackbar.show}
-        autoHideDuration={5000}
+    return(
+  <div className='App'>
+    <ThemeProvider theme={custom_theme} className='container'>
+      <Router>
+        <Switch>
+          <Route
+            exact
+            path='/login'
+            component={() => (
+              <Login
+                setCircle={(value) => setCircle(value)}
+                setSnackbar={(value) => setSnackbar(value)}
+              />
+            )}
+          />
+          <Route
+            exact
+            path='/register'
+            component={() => (
+              <Register
+                setCircle={(value) => setCircle(value)}
+                setSnackbar={(value) => setSnackbar(value)}
+              />
+            )}
+          />
+          <Route
+            exact
+            path='/'
+            component={() => (
+              <Home
+                setCircle={(value) => setCircle(value)}
+                setSnackbar={(value) => setSnackbar(value)}
+              />
+            )}
+          />
+          <Route
+            exact
+            path='/user/:userId'
+            component={() => (
+              <UserProfile
+                setCircle={(value) => setCircle(value)}
+                setSnackbar={(value) => setSnackbar(value)}
+              />
+            )}
+          />
+          <Route
+            exact
+            path='/uni/:uniId'
+            component={() => (
+              <UniProfile
+                setCircle={(value) => setCircle(value)}
+                setSnackbar={(value) => setSnackbar(value)}
+              />
+            )}
+          />
+          <Route
+            path='/club/:clubId'
+            component={() => (
+              <Club
+                setCircle={(value) => setCircle(value)}
+                setSnackbar={(value) => setSnackbar(value)}
+              />
+            )}
+          />
+          <Route
+            exact
+            path='/club'
+            component={() => (
+              <Club
+                setCircle={(value) => setCircle(value)}
+                setSnackbar={(value) => setSnackbar(value)}
+              />
+            )}
+          />
+          <Route
+            exact
+            path='/studygroup/:studygroupId'
+            component={() => (
+              <StudyGroup
+                setCircle={(value) => setCircle(value)}
+                setSnackbar={(value) => setSnackbar(value)}
+              />
+            )}
+          />
+          <Route
+            exact
+            path='/market'
+            component={() => (
+              <Market
+                setCircle={(value) => setCircle(value)}
+                setSnackbar={(value) => setSnackbar(value)}
+              />
+            )}
+          />
+          <Route
+            exact
+            path='/market/:itemId'
+            component={() => (
+              <Market
+                setCircle={(value) => setCircle(value)}
+                setSnackbar={(value) => setSnackbar(value)}
+              />
+            )}
+          />
+          <Route
+            exact
+            path='/messenger'
+            component={() => (
+              <Messenger
+                setCircle={(value) => setCircle(value)}
+                setSnackbar={(value) => setSnackbar(value)}
+              />
+            )}
+          />
+          <Route exact path='/settings' component={Settings} />
+          <Route exact path='/error' component={Error} />
+        </Switch>
+      </Router>
+    </ThemeProvider>
+    <Backdrop
+      sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      open={circle}
+    >
+      <CircularProgress color='inherit' />
+    </Backdrop>
+    <Snackbar
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'center',
+      }}
+      open={snackbar.show}
+      autoHideDuration={5000}
+      onClose={() => setSnackbar(false)}
+    >
+      <Alert
         onClose={() => setSnackbar(false)}
+        severity={snackbar.severity}
+        sx={{ width: '100%' }}
       >
-        <Alert
-          onClose={() => setSnackbar(false)}
-          severity={snackbar.severity}
-          sx={{ width: '100%' }}
-        >
-          {snackbar.text}
-        </Alert>
-      </Snackbar>
-    </div>
-  // )
-// }
+        {snackbar.text}
+      </Alert>
+    </Snackbar>
+  </div>
+)}}
+
+  // constructor(props) {
+  //   super(props)
+  //   this.state = { loading: false, msg: null }
+  // }
+  //
+  // handleClick = api => e => {
+  //   e.preventDefault()
+  //
+  //   this.setState({ loading: true })
+  //   fetch("/.netlify/functions/" + api)
+  //     .then(response => response.json())
+  //     .then(json => this.setState({ loading: false, msg: json.msg }))
+  // }
+  //
+  // render() {
+  //   const { loading, msg } = this.state
+  //
+  //   return (
+  //     <p>
+  //       <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
+  //       <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
+  //       <br />
+  //       <span>{msg}</span>
+  //     <p>
+  //   )
+  // }
 }
+
 
 export default App
